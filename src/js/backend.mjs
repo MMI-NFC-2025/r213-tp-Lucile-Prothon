86eeb459e52769fbb8a40bff2c2ca1faf7c582f0
Lucile-Prothon <lucile.prothon@edu.univ-fcomte.fr>
2026-02-06 15:18:51 +0100

    Ajout route dynamique surface

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -27,3 +27,15 @@ export async function getOffre(id) {
         return {};
     }
 }
+
+export async function getSurface(surface) {
+    try {
+        const data = await db.collection('maison').getFullList({
+            filter: `surface = ${surface}`,
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la maison', error);
+        return {};
+    }
+}
